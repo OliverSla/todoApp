c7e113aab7ebde5682e006d69f88ab656e12b815
Peester <oliversladkovsky@gmail.com>
2022-11-13 18:58:52 +0100

    todo update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,17 +7,14 @@ let taskWrapper = document.querySelector(".taskWrapper")
 let rightImg = document.querySelector(".rightImg")
 let settingsIcon = document.querySelector(".settingsIcon")
 let settingBlock = document.querySelector(".settingBlock")
-
-
-settingsIcon.addEventListener("click", function(e){
-
-    settingBlock.style.display = "flex"
-
-
-})
+let settingWrapper = document.querySelector(".settingWrapper")
 
 
 
+
+/*
+CHECK BOX 
+*/
 checkbox.addEventListener("click", function(){
 
 let styleOfcheckIcon = getComputedStyle(checkIcon)
@@ -28,17 +25,43 @@ if(styleOfcheckIcon.display === "none"){
     return checkIcon.style.display = "none"
 }
 
-
 })
 
-
-
-
+/*
+If hover show setting icon
+*/
 taskWrapper.addEventListener("mouseover", function(e){
     rightImg.style.display = "flex"
+
 })
 
 taskWrapper.addEventListener("mouseout", function(e){
     rightImg.style.display = "none"
+
 })
+
+
+
+/*
+If click setting icon show settings
+*/
+
+settingsIcon.addEventListener("click", function(e){
+    settingWrapper.style.display = "flex"
+})
+
+settingBlock.addEventListener("mouseover", function(e){
+    rightImg.style.display = "flex"
+    settingWrapper.style.display = "flex"
+})
+
+settingWrapper.addEventListener("mouseout", function(e){
+    settingWrapper.style.display = "none"
+})
+
+settingWrapper.addEventListener("mouseout", function(e){
+    settingWrapper.style.display = "flex"
+})
+
+
 
